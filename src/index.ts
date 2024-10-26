26e612ed662b10d3c5eda7f6b17585381bd82ddb
Exteri0 <omarfaramawy19@gmail.com>
2024-10-26 19:37:32 -0400

    some more testing

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });  */
 
 console.log("Before registering routes");
-console.log("testing the github actionSDASDSADASDASDasdasdass");
+console.log("GITHUBA CTIONS TESTING AGAINAS DASDAS");
 routes(app);
 console.log("Routes registered");
 
